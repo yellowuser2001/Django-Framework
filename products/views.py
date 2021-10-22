f5b6b38e029820103b9867512f21ca2b736e63de
Nina Skryabina <alekseevnanina38@gmail.com>
2021-10-22 19:48:27 +0300

    refactor: Организовать загрузку динамического контента в контроллер

--- products/views.py
+++ products/views.py
@@ -1,7 +1,9 @@
+import os
+import json
+
 from django.shortcuts import render
 
-
-# Create your views here.
+MODULE_DIR = os.path.dirname(__file__)
 
 
 def index(request):
@@ -13,28 +15,9 @@ def index(request):
 
 
 def products(request):
+    file_path = os.path.join(MODULE_DIR, 'fixtures/goods.json')
     context = {
         'title': 'GeekShop - Каталог',
-        'products': [
-            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00',
-             'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
-             'image': 'vendor/img/products/Adidas-hoodie.png'},
-            {'name': 'Синяя куртка The North Face', 'price': '23 725,00',
-             'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
-             'image': 'vendor/img/products/Blue-jacket-The-North-Face.png'},
-            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00',
-             'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
-             'image': 'vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png'},
-            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00',
-             'description': 'Плотная ткань. Легкий материал.',
-             'image': 'vendor/img/products/Black-Nike-Heritage-backpack.png'},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00',
-             'description': 'Гладкий кожаный верх. Натуральный материал.',
-             'image': 'vendor/img/products/Black-Dr-Martens-shoes.png'},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2 890,00',
-             'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.',
-             'image': 'vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
-        ]
+        'products': json.load(open(file_path, encoding='utf-8'))
     }
-
     return render(request, 'products/products.html', context)
